1b03d6dcb978885be87784b396e5e9f443a58bba
VitorVGDev <vgvital@edu.unifil.br>
2025-06-16 12:26:56 -0300

    Add new image asset 'Group 4.png' to the project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,71 @@
 import styles from '../components/About.module.css';
+import img_group1 from '../assets/Group 1.png';
+import img_group2 from '../assets/Group 2.png';
+import img_group3 from '../assets/Group 3.png';
+import img_group4 from '../assets/Group 4.png';
 
 function About () {
     return (
-        <div>
-            
+        <div className={styles.about_container}>
+            <div>
+                <div className={styles.about_grid}>
+                    <div className={styles.header_about}>
+                        <h1>Work Experience</h1>
+                    </div>
+                    <div className={styles.box1}>
+                        <div>
+                            <img src={img_group1}
+                            className={styles.img_about}
+                            />
+                        </div>
+                        <div>
+                            <h1>CIB on the Mobile</h1>
+                            <p>Take your client onboard seamlessly by our amazing
+                            tool of digital onboard process.</p>
+                            <button className={styles.button_about}>Learn More</button>
+                        </div>
+                    </div>
+                    <div className={styles.box2}>
+                        <div>
+                            <img src={img_group2} 
+                            className={styles.img_about}
+                            />
+                        </div>
+                        <div>
+                            <h1>CIB on the Mobile</h1>
+                            <p>Take your client onboard seamlessly by our amazing
+                            tool of digital onboard process.</p>
+                            <button className={styles.button_about}>Learn More</button>
+                        </div>
+                    </div>
+                    <div className={styles.box3}>
+                        <div>
+                            <img src={img_group3}
+                            className={styles.img_about}
+                            />
+                        </div>
+                        <div>
+                            <h1>CIB on the Mobile</h1>
+                            <p>Take your client onboard seamlessly by our amazing
+                            tool of digital onboard process.</p>
+                            <button className={styles.button_about}>Learn More</button>
+                        </div>
+                    </div>
+                    <div className={styles.box4}>
+                        <div>
+                            <img src={img_group4} 
+                            className={styles.img_about}
+                            />
+                        </div>
+                        <div>
+                            <h1>CIB on the Mobile</h1>
+                            <p>Take your client onboard seamlessly by our amazing
+                            tool of digital onboard process.</p>
+                            <button className={styles.button_about}>Learn More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
